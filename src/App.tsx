35934047e4a20dbebf1d722089a0478de664bb05
vitorhugomarcal <vitorhugomarcal@gmail.com>
2022-10-15 00:50:45 -0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { ApolloProvider } from "@apollo/client"
+import { client } from "./lib/apollo"
+import { Router } from "./Router"
+
 function App() {
   return (
-   <h1>Hello World</h1>
+    <ApolloProvider client={client}>
+      <Router />
+    </ApolloProvider>
   )
 }
 
